4d248a2efae40fdd21af199776d12b816f1fafdc
xuefeng <1332667077@qq.com>
2021-12-29 16:41:53 +0800

    [add] DBSCAN

--- clustering/density_based_methods.py
+++ clustering/density_based_methods.py
@@ -1,11 +1,85 @@
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import OPTICS
-
+import math
+import matplotlib.pyplot as plt
+from scipy.spatial import KDTree
+import random
+import numpy as np
 def dbscan_clustering(data):
-    dbscan = DBSCAN(eps=0.01)
+    dbscan = DBSCAN(eps=2.0)
     label = dbscan.fit_predict(data)
     return label
 
 def optics_clustering(data):
     optics = OPTICS()
     return optics.fit_predict(data)
+
+
+def dist(a, b):
+    len_a = len(a)
+    ans = 0
+    for i in range(len_a):
+        ans = ans + (a[i] - b[i]) * (a[i] - b[i])
+    return math.sqrt(ans)
+
+def getCore(dataset , eps , min_samples):
+    core = {}
+    KdTree = KDTree(dataset)
+    len_d = len(dataset)
+    for i in range(len_d):
+        temp = np.sum((dataset - dataset[i]) ** 2, axis=1) ** 0.5
+        neibor = np.argwhere(temp <= eps).flatten().tolist()
+        #neibor = KdTree.query_ball_point(dataset[i],eps)
+        if len(neibor) > min_samples :
+            core[i] = neibor
+    return core
+
+def dbscan(dataset,eps = 2.0,min_samples = 30):
+    len_d = len(dataset)
+    '''
+    dis_k = []
+
+    for idx1 in range(len_d):
+        dis = []
+        for idx2 in range(len_d):
+            dis.append(dist(dataset[idx1],dataset[idx2]))
+        dis = sorted(dis)
+        dis_k.append(dis[4])
+    dis_k = sorted(dis_k)
+    x1 = [i for i in range(len_d)]
+    plt.plot(x1, dis_k, label='Frist line', linewidth=3, color='r', marker='o',markerfacecolor='blue', markersize=12)
+    plt.xlabel('Plot Number')
+    plt.ylabel('distance')
+    plt.title('Eps Selection')
+    plt.legend()
+    plt.show()
+    '''
+    unvisited = [0 for i in range(len_d)]
+    core_neiborhood = getCore(dataset , eps ,min_samples)
+    #print(core_neiborhood)
+    cluster = [-1 for i in range(len_d)] #记录结果
+    cluster_id = 0
+    for core_id,neibor in core_neiborhood.items(): #遍历核心点
+        if unvisited[core_id] == 1: #如果已经访问过
+            continue
+        new_cluster = [core_id]
+        core_queue = [core_id]
+        unvisited[core_id] = 1
+        while len(core_queue): #对核心点进行bfs
+            x = core_queue.pop()
+            neibor = core_neiborhood[x]
+            for n_id in neibor: #核心点邻域
+                if unvisited[n_id] == 0: #如果发现没访问过
+                    unvisited[n_id] = 1
+                    new_cluster.append(n_id)
+                    if n_id in core_neiborhood : #如果发现邻域内核心点
+                        core_queue.append(n_id)
+        for kk in new_cluster:
+            cluster[kk] = cluster_id
+        cluster_id = cluster_id + 1
+
+    #print(cluster)
+    print(cluster_id)
+    return cluster
+
+
--- clustering/hierarchical_based_methods.py
+++ clustering/hierarchical_based_methods.py
@@ -1,5 +1,70 @@
 from sklearn.cluster import AgglomerativeClustering
+import math
 
 def agglomerative_clustering(data):
     cluster = AgglomerativeClustering(n_clusters=5)
     return cluster.fit_predict(data)
+
+def dist(a, b):
+    len_a = len(a)
+    ans = 0
+    for i in range(len_a):
+        ans = ans + (a[0] - b[0]) * (a[0] - b[0])
+    return math.sqrt(ans)
+
+def find_Min(M):
+    min = 1000
+    x = 0; y = 0
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            if i != j and M[i][j] < min:
+                min = M[i][j];x = i; y = j
+    return (x, y, min)
+
+#dist_min
+def dist_min(Ci, Cj):
+    return min(dist(i, j) for i in Ci for j in Cj)
+
+def dist_max(Ci, Cj):
+    return max(dist(i, j) for i in Ci for j in Cj)
+
+def dist_avg(Ci, Cj):
+    return sum(dist(i, j) for i in Ci for j in Cj)/(len(Ci)*len(Cj))
+
+def AGNES(dataset, k=5):
+    #初始化C和M
+    dist_tmp = dist_min
+    C = [];M = []
+    for idx,i in enumerate(dataset):
+        Ci = []
+        Ci.append(idx)
+        C.append(Ci)
+    for i in C:
+        Mi = []
+        for j in C:
+            Mi.append(dist_tmp(dataset[i], dataset[j]))
+        M.append(Mi)
+    q = len(dataset)
+    len_d = q
+    #合并更新
+    while q > k:
+        x, y, min = find_Min(M)
+        C[x].extend(C[y])
+        C = C[:y] + C[y+1:]
+        #C.remove(C[y])
+        M = []
+        for i in C:
+            Mi = []
+            for j in C:
+                Mi.append(dist_tmp(dataset[i], dataset[j]))
+            M.append(Mi)
+        q -= 1
+    c_id = 0
+    clus = [0 for i in range(len_d)]
+    for k in C:
+        for kk in k:
+            #print(kk)
+            clus[kk] = c_id
+        c_id = c_id + 1
+    #print(clus)
+    return clus
